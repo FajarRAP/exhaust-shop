861285063c66d32eff7885dbe10fea4f4e008731
Fajar Riansyah <fajary781@gmail.com>
2026-03-02 03:22:35 +0700

    feat (product routes): add product management routes for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', ['product' => Product::latest()->first()]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -34,6 +36,21 @@ Route::prefix('admin')
             ->name('categories.update');
         Route::delete('categories/{category}', [CategoryController::class, 'destroy'])
             ->name('categories.destroy');
+
+        Route::get('products', [ProductController::class, 'index'])
+            ->name('products.index');
+        Route::get('products/{product}', [ProductController::class, 'show'])
+            ->name('products.show');
+        Route::get('products/create', [ProductController::class, 'create'])
+            ->name('products.create');
+        Route::post('products', [ProductController::class, 'store'])
+            ->name('products.store');
+        Route::get('products/{product}/edit', [ProductController::class, 'edit'])
+            ->name('products.edit');
+        Route::put('products/{product}', [ProductController::class, 'update'])
+            ->name('products.update');
+        Route::delete('products/{product}', [ProductController::class, 'destroy'])
+            ->name('products.destroy');
     });
 
 require __DIR__ . '/auth.php';
