7be5da0742c635b53c8276f2d2338d23e867fdd7
Fajar Riansyah <fajary781@gmail.com>
2026-03-29 18:56:33 +0700

    feat (database seeder): register ProductSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Cart;
-use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
+            ProductSeeder::class,
         ]);
 
         User::factory()->create([
@@ -30,14 +28,5 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@example.com',
         ]);
-
-        $randomProducts = Product::inRandomOrder()->take(5)->get();
-
-        foreach ($randomProducts as $product) {
-            Cart::factory()->create([
-                'user_id' => 1, // customer
-                'product_id' => $product->id,
-            ]);
-        }
     }
 }
